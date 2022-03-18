a8e24bda8c473f2aabd005b3d7227d8ffe9e3a9f
Pedro Dominguez <pdomingb@everis.com>
2022-03-18 18:30:19 -0500

    add api demo

---
+++ TipoCambioAPI/Models/MonedaDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TipoCambioAPI.Models
+{
+    public class MonedaDbContext:DbContext
+    {
+        public MonedaDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Moneda> Monedas{ get; set; }
+
+    }
+}
---
+++ TipoCambioAPI/Models/TipoCambio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TipoCambioAPI.Models
+{
+    public class TipoCambio
+    {
+        public decimal monto{ get; set; }
+        public decimal montoCambio{ get; set; }
+        public string origen{ get; set; }
+        public string destino{ get; set; }
+        public decimal tipoDeCambio{ get; set; }
+    }
+}
